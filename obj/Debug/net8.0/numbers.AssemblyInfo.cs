e360ae1e6edf593f0c1ab44ae06708a601dccdcc
asplicity <ticobeier@gmail.com>
2024-06-01 20:27:35 +0200

    b

--- obj/Debug/net8.0/numbers.AssemblyInfo.cs
+++ obj/Debug/net8.0/numbers.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("numbers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4050b2c88f1260d7469cc0b9345cd7e0d8cfdb49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7e7657908605a9cf817667e1564f69ffd874766")]
 [assembly: System.Reflection.AssemblyProductAttribute("numbers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("numbers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
